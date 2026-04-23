bb6f1626f2f414e5a0d38c6ca5b1ba7e3edd043d
seesawmoons <140035377+seesawmoons@users.noreply.github.com>
2026-04-23 01:34:06 +0000

    progress bar to show where we messed up

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,13 +23,17 @@ for x in range(len(ships)):
 
 rows, cols = (S, S)
 rank = [[0]*cols]*rows
-
+st.text("")
+progressBar = st.text("PROGRESS : 0/" + str(total) + " ranked")
 choose1 = st.button("i like the first one")
 chooseTie = st.button("🤷‍♀️")
 choose2 = st.button("i like the second one")
 
 Option1 = st.text("")
 Option2 = st.text("")
+total = int((S*S - S) / 2)
+
+progress = 0
 
 # buttons go here
 finished = False
@@ -37,10 +41,12 @@ selection = False
 while (not finished) and (not selection):
     finished = True
     selection = True
+    progress = total
     for i in range(S):
         for j in range(S):
             if rank[i][j] == 0:
                 finished = False
+                progress -= 1
     P1 = random.randint(0, S-1)
     while 0 not in rank[P1]:
         P1 = (P1 + 1) % S
@@ -48,6 +54,7 @@ while (not finished) and (not selection):
 
     while (P2 == P1) or (rank[P1][P2] != 0):
         P2 = (P2 + 1) % S
+    progressBar.write("PROGRESS : " + str(progress) + "/" + str(total) + " ranked")
     Option1.write(ships[P1] + "!!")
     Option2.write(ships[P2] + "!!")
 
@@ -129,7 +136,7 @@ for i in range(S):
     maxWins -= 1
     for j in range(S):
         if wins[j] == maxWins:
-            st.write(noEntries + 1, ships[j])
+            st.write(str(noEntries + 1) + ". " + ships[j])
             tentEntries += 1
 
 
