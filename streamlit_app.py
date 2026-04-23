fca2e2f1d247e41b5c64ae4a2898cfe2c5a8e8c0
seesawmoons <140035377+seesawmoons@users.noreply.github.com>
2026-04-23 00:47:16 +0000

    we try again

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,31 +3,44 @@ import streamlit as st
 
 st.title("fight fight fight")
 st.write(
-    "if something breaks, don't sue me! no graphics bc im a loser"
+    "if something breaks, don't sue me! no graphics bc im a loser\n"
 )
 
 charas = ["One", "Two", "Three", "Four"]
 S = int((len(charas)*len(charas) - len(charas)) / 2 )
 # 4 charas = 6 ships
 
-ships = ["yes"]*S
+ships = ["yes"] * S
 a = 0
 b = 1
-for x in ships:
-    #something loop
-	x = charas[a] + " x " + charas[b]
+for x in range(len(ships)):
+	ships[x] = charas[a] + " x " + charas[b]
 	b += 1
 	if (b == len(charas)):
 		a += 1
 		b = a + 1
 		
-	
 
 rows, cols = (S, S)
 rank = [[0]*cols]*rows
 
+choose1 = st.button("i like the first one")
+choose2 = st.button("i like the second one")
+chooseTie = st.button("🤷‍♀️")
+
+Option1 = st.text("")
+Option2 = st.text("")
+
 # buttons go here
-while (0 in rank):
+finished = False
+selection = True
+while (not finished) and (selection == True):
+    finished = True
+    selection = False
+    for i in range(S):
+        for j in range(S):
+            if rank[i][j] == 0:
+                finished = False
     P1 = random.randint(0, S-1)
     while 0 not in rank[P1]:
         P1 = (P1 + 1) % S
@@ -35,18 +48,71 @@ while (0 in rank):
 
     while (P2 == P1) or (rank[P1][P2] != 0):
         P2 = (P2 + 1) % S
-    st.write("Option 1: " + ships[P1])
-    st.write("Option 2: " + ships[P2])
-    if st.button("First one"):
+    Option1.write(ships[P1] + "!!")
+    Option2.write(ships[P2] + "!!")
+    if choose1:
         rank[P1][P2] = 1
         rank[P2][P1] = 3
-    elif st.button("🤷‍♀️"):
+        if rank[i][P1] == 1:
+                rank[i][P2] = 1
+                rank[P2][i] = 3
+        elif rank[i][P1] == 2:
+            rank[i][P2] = 1
+            rank[P2][i] = 3
+
+        if rank[i][P2] == 3:
+            rank[i][P1] = 3
+            rank[P1][i] = 1
+        elif rank[i][P2] == 2:
+            rank[i][P1] = 3
+            rank[P1][i] = 1
+        selection = True
+    elif chooseTie:
         rank[P1][P2] = 2
         rank[P2][P1] = 2
-    elif st.button("Second one"):
+        for i in range(S):
+            if rank[i][P2] == 1: # Wins against P2
+                rank[i][P1] = 1
+                rank[P1][i] = 3
+            elif rank[i][P2] == 3: # Loses against P2
+                rank[i][P1] = 3
+                rank[P1][i] = 1
+            elif rank[i][P2] == 2:
+                rank[i][P1] = 2
+                rank[P1][i] = 2
+            
+            if rank[i][P1] == 1: # Wins against P1
+                rank[i][P2] = 1
+                rank[P2][i] = 3
+            elif rank[i][P1] == 3: # Loses against P1
+                rank[i][P2] = 3
+                rank[P2][i] = 1
+            elif rank[i][P1] == 2:
+                rank[i][P2] = 2
+                rank[P2][i] = 2
+        selection = True
+    elif choose2:
         rank[P2][P1] = 1
         rank[P1][P2] = 3
+        for i in range(S):
+            if rank[i][P2] == 1:
+                rank[i][P1] = 1
+                rank[P1][i] = 3
+            elif rank[i][P2] == 2:
+                rank[i][P1] = 1
+                rank[P1][i] = 3
 
+            if rank[i][P1] == 3:
+                rank[i][P2] = 3
+                rank[P2][i] = 1
+            elif rank[i][P1] == 2:
+                rank[i][P2] = 3
+                rank[P2][i] = 1
+        selection = True
+    else:
+        selection = False
+
+st.title("the results!")
 
 
 
